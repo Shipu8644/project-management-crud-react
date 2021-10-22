55c3cd53475ddad4896e517e64877688c07333ce
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-23 03:25:16 +0600

    products showing

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Products = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:7000/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
     return (
         <div>
-            <h1>This is products</h1>
+            <h1>Number of products: {products.length} </h1>
+            <ol>
+                {
+                    products.map(product => <li key={product._id}>
+                        name={product.name}
+                        <br />
+                        price={product.price}
+                        <br />
+                        quantity={product.quantity}
+                        <br />
+                        <br />
+                        <button>Update</button>
+                        <br />
+                        <button>Delete</button>
+                        <br />
+                        <br />
+                    </li>)
+                }
+
+            </ol>
         </div>
     );
 };
